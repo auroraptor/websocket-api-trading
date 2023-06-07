e18ec8d48c07637a30ff3c471afd513db4713646
auroraptor <interestingbutno@yandex.ru>
2023-06-07 23:07:17 +0300

    feature: enhanced type Envelope
    Changed the type of 'message' from 'object' to '{ orders: OrderData[] }' in the 'Envelope' type.

--- src/Models/Base.ts
+++ src/Models/Base.ts
@@ -1,10 +1,11 @@
 import Decimal from "decimal.js";
 import {ClientMessage} from "./ClientMessages";
 import {ServerMessage} from "./ServerMessages";
+import { OrderData } from "../OrdersTable";
 
 export interface Envelope {
     messageType: ClientMessage | ServerMessage
-    message: object
+    message: { orders: OrderData[] }
 }
 
 export interface Message {
