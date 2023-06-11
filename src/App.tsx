1739711a6031f8d6d3711c614054d220c43ee5f8
auroraptor <interestingbutno@yandex.ru>
2023-06-11 22:52:55 +0300

    feat: add new props to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { useEffect, useState } from 'react';
-import WSConnector from './WSClient';
-import './App.css';
-import Ticker from './Ticker';
-import OrdersTable from './OrdersTable';
-import { OrderData } from './OrdersTable';
+import { useEffect, useState } from "react";
+import WSConnector from "./WSClient";
+import "./App.css";
+import Ticker from "./Ticker";
+import OrdersTable from "./OrdersTable";
+import { OrderData } from "./OrdersTable";
+import Decimal from "decimal.js";
 
 function App() {
   const [orders, setOrders] = useState<OrderData[]>([]);
+  const [wsConnector, setWsConnector] = useState<WSConnector | null>(null);
 
   useEffect(() => {
-    const wsConnector = new WSConnector();
-    wsConnector.connect((data: OrderData[]) => {
+    const connector = new WSConnector();
+    setWsConnector(connector);
+    connector.connect((data: OrderData[]) => {
       setOrders(data);
     });
   }, []);
 
   return (
     <div className="App">
-      <Ticker orders={orders} onOrderSubmit={(a) => console.log('App get: ', a)}/>
+      <Ticker price={new Decimal(1000)} onOrderSubmit={(order) => console.log(order)} />
       <OrdersTable orders={orders} />
     </div>
   );
